034699ffc0325b077dda661fa440422c822b8530
Gabriel <Gabriel@gmail.com>
2021-03-13 16:33:04 -0400

    Primer intento seccion contacto responsive

---
+++ contacto/script.js
@@ -0,0 +1,7 @@
+const act = document.getElementById('header__bar--img');
+const act_2 = document.getElementById('header__bg');
+
+function cambio() {
+    act.classList.toggle('act');
+    act_2.classList.toggle('act_2');
+};
